d25d5bca751fd8b3d4ab576a1f87d714315ad34d
Giacomo "Jack" Pedemonte <160411435+ElSenorJack@users.noreply.github.com>
2024-03-20 17:56:35 +0100

    Raycasting; Enemy Hp; Shoot Particles
    made a Weapon script for raycasting, implemented Enemy health, muzzle flash particles, hit holes and sparks on Hit.

---
+++ Assets/Scripts/Weapon.cs
@@ -0,0 +1,54 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Experimental.GraphView;
+using UnityEngine;
+
+public class Weapon : MonoBehaviour
+{
+    [SerializeField] Camera FPCamera;
+    [SerializeField] float range = 100f;
+    [SerializeField] float damage = 20f;
+    [SerializeField] ParticleSystem muzzleFlash;
+    [SerializeField] GameObject hitHole;
+    [SerializeField] GameObject hitSparks;
+
+
+    void Update()
+    {
+        if (Input.GetButtonDown("Fire1"))
+        { Shoot();  }
+    }
+
+    private void Shoot()
+    {
+        Raycasting();
+        PlayMuzzleFlash();
+    }
+
+    private void PlayMuzzleFlash()
+    {
+        muzzleFlash.Play();
+    }
+
+    private void Raycasting()
+    {
+        RaycastHit hit;
+        if (Physics.Raycast(FPCamera.transform.position, FPCamera.transform.forward, out hit, range))
+        {
+            ImpactHit(hit);
+            EnemyHealth target = hit.transform.GetComponent<EnemyHealth>();
+            if (target == null) { return; }
+            target.TakeDamage(damage);
+        }
+        else { return; }
+    }
+
+    private void ImpactHit(RaycastHit hit)
+    {
+       GameObject impactSparks = Instantiate(hitSparks, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impactSparks, 1);
+       GameObject impactHole = Instantiate(hitHole, hit.point, Quaternion.LookRotation(hit.normal));
+        Destroy(impactHole, 3);
+    }
+}
