6d932fef89bd93ecd916cb768244766afdb42fcf
Giacomo "Jack" Pedemonte <160411435+ElSenorJack@users.noreply.github.com>
2024-03-27 19:09:02 +0100

    Scene Loader UI

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -61,7 +61,7 @@ public class EnemyAI : MonoBehaviour
 
     private void FaceTarget()
     {
-        Vector3 direction = (target.position - transform.position.normalized);
+        Vector3 direction = (target.position - transform.position).normalized;
         Quaternion lookRotation = Quaternion.LookRotation(new Vector3(direction.x, 0, direction.z));
         transform.rotation = Quaternion.Slerp(transform.rotation, lookRotation, Time.deltaTime * turnSpeed);
     }
